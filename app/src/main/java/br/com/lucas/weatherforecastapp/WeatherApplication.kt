062acdb189d502e28b152e57b55845a4a7114a49
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-03 16:14:58 -0300

    Setting up the DI structures

---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package br.com.lucas.weatherforecastapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application() {}
---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/di/AppModule.kt
@@ -0,0 +1,9 @@
+package br.com.lucas.weatherforecastapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {}
