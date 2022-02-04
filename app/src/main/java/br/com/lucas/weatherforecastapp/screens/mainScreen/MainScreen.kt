aebcb9e5b6501eaf6cede8466c51bf5b33094ad1
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 14:35:45 -0300

    Creating the Weather App Bar on Main Screen

--- app/src/main/java/br/com/lucas/weatherforecastapp/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/screens/mainScreen/MainScreen.kt
@@ -1,25 +1,25 @@
 package br.com.lucas.weatherforecastapp.screens.mainScreen
 
+import android.util.Log
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import br.com.lucas.weatherforecastapp.data.DataOrException
 import br.com.lucas.weatherforecastapp.model.WeatherObject
+import br.com.lucas.weatherforecastapp.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(
     navController: NavController,
     viewModel: MainViewModel = hiltViewModel()
 ) {
-    ShowData(viewModel)
-}
-
-@Composable
-fun ShowData(viewModel: MainViewModel) {
-
     val weatherData =
         produceState<DataOrException<WeatherObject, Boolean, Exception>>(
             initialValue = DataOrException(loading = true)
@@ -28,6 +28,29 @@ fun ShowData(viewModel: MainViewModel) {
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "MainScreen ${weatherData.data!!}")
+        MainScaffold(weatherObject = weatherData.data!!, navController = navController)
     }
+}
+
+@Composable
+fun MainScaffold(weatherObject: WeatherObject, navController: NavController) {
+
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = weatherObject.city.name + ", ${weatherObject.city.country}",
+            navController = navController,
+            elevation = 4.dp,
+        )
+    }) {
+        MainContent(data = weatherObject)
+    }
+
+}
+
+@Composable
+fun MainContent(data: WeatherObject) {
+
+    Text(text = data.city.name)
+
+
 }
---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/widgets/WeatherAppBar.kt
@@ -0,0 +1,64 @@
+package br.com.lucas.weatherforecastapp.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import br.com.lucas.weatherforecastapp.R
+
+@Composable
+fun WeatherAppBar(
+    title: String,
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onSecondary,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp
+                )
+            )
+        },
+        actions = {
+            if (isMainScreen) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = stringResource(R.string.label_search_icon))
+                }
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = stringResource(R.string.label_more_icon))
+                }
+            } else Box {}
+        },
+        navigationIcon = {
+            if (icon != null) {
+                IconButton(onClick = { onButtonClicked() }) {
+                    Icon(imageVector = icon, contentDescription = null)
+                }
+            }
+        },
+        backgroundColor = Color.Transparent,
+        elevation = elevation
+    )
+}
