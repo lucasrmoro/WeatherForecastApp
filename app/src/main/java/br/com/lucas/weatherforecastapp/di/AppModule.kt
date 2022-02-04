e679b4de49eaf4d6bf7066b1dcf2d0ed9c54c844
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 10:42:23 -0300

    Providing the dependencies on AppModule

--- app/src/main/java/br/com/lucas/weatherforecastapp/di/AppModule.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/di/AppModule.kt
@@ -1,9 +1,29 @@
 package br.com.lucas.weatherforecastapp.di
 
+import br.com.lucas.weatherforecastapp.network.WeatherApi
+import br.com.lucas.weatherforecastapp.repository.WeatherRepository
+import br.com.lucas.weatherforecastapp.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class AppModule {}
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(): WeatherApi = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(WeatherApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideRepository(weatherApi: WeatherApi): WeatherRepository = WeatherRepository(weatherApi)
+}
