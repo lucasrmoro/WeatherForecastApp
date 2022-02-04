470b11998155116d2cf451d202d3a4fb6591f555
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 09:19:46 -0300

    Creating an object for constants

---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package br.com.lucas.weatherforecastapp.utils
+
+import br.com.lucas.weatherforecastapp.BuildConfig
+
+object Constants {
+    const val BASE_URL = "https://api.openwathermap.org/"
+    const val API_KEY = BuildConfig.API_KEY
+
+}
