3f410ed94d3641f6f8119518cdf601e62b82c175
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 15:58:54 -0300

    Creating a Utils file to keep util functions

---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/utils/Utils.kt
@@ -0,0 +1,21 @@
+package br.com.lucas.weatherforecastapp.utils
+
+import java.text.SimpleDateFormat
+
+fun Int.formatDate(): String {
+    val sdf = SimpleDateFormat("EEE, MMM d")
+    val date = java.util.Date(this.toLong() * 1000)
+
+    return sdf.format(date)
+}
+
+fun Int.formatDateTime(): String {
+    val sdf = SimpleDateFormat("hh:mm:aa")
+    val date = java.util.Date(Int.toLong() * 1000)
+
+    return sdf.format(date)
+}
+
+fun Double.formatDecimals(): String {
+    return " %.0f".format(this)
+}
