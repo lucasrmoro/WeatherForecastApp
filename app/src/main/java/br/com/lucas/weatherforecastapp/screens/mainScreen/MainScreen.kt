445018fc9550efb23400b0b0f98a7703410a95db
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 15:59:49 -0300

    Creating a top circle on Main Screen to show the current weather

--- app/src/main/java/br/com/lucas/weatherforecastapp/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/screens/mainScreen/MainScreen.kt
@@ -1,19 +1,27 @@
 package br.com.lucas.weatherforecastapp.screens.mainScreen
 
-import android.util.Log
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import br.com.lucas.weatherforecastapp.R
 import br.com.lucas.weatherforecastapp.data.DataOrException
 import br.com.lucas.weatherforecastapp.model.WeatherObject
+import br.com.lucas.weatherforecastapp.utils.formatDate
+import br.com.lucas.weatherforecastapp.utils.formatDecimals
 import br.com.lucas.weatherforecastapp.widgets.WeatherAppBar
+import coil.compose.rememberImagePainter
 
 @Composable
 fun MainScreen(
@@ -23,7 +31,7 @@ fun MainScreen(
     val weatherData =
         produceState<DataOrException<WeatherObject, Boolean, Exception>>(
             initialValue = DataOrException(loading = true)
-        ) { value = viewModel.getWeatherData(city = "Seattle") }.value
+        ) { value = viewModel.getWeatherData(city = "uruguaiana") }.value
 
     if (weatherData.loading == true) {
         CircularProgressIndicator()
@@ -44,13 +52,59 @@ fun MainScaffold(weatherObject: WeatherObject, navController: NavController) {
     }) {
         MainContent(data = weatherObject)
     }
-
 }
 
 @Composable
 fun MainContent(data: WeatherObject) {
+    val weatherItem = data.list.first()
+    val imageUrl = "https://openweathermap.org/img/wn/${weatherItem.weather.first().icon}.png"
 
-    Text(text = data.city.name)
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = weatherItem.dt.formatDate(),
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
 
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
 
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                WeatherStateImage(imageUrl = imageUrl)
+
+                Text(
+                    text = weatherItem.temp.day.formatDecimals() + "°",
+                    style = MaterialTheme.typography.h4,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherItem.weather.first().main, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberImagePainter(imageUrl),
+        contentDescription = stringResource(R.string.label_weather_icon),
+        modifier = Modifier.size(80.dp)
+    )
 }
--- app/src/main/java/br/com/lucas/weatherforecastapp/widgets/WeatherAppBar.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/widgets/WeatherAppBar.kt
@@ -1,19 +1,16 @@
 package br.com.lucas.weatherforecastapp.widgets
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
