32c76d221678b6f9882a64d9ea5e91468d3f933f
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-04 10:40:35 -0300

    Creating the WeatherApi file to get the data

---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/network/WeatherApi.kt
@@ -0,0 +1,17 @@
+package br.com.lucas.weatherforecastapp.network
+
+import br.com.lucas.weatherforecastapp.model.WeatherObject
+import br.com.lucas.weatherforecastapp.utils.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+import javax.inject.Singleton
+
+@Singleton
+interface WeatherApi {
+    @GET("data/2.5/forecast/daily")
+    suspend fun getWeather(
+        @Query("q") query: String,
+        @Query("units") units: String = "imperial",
+        @Query("appid") appid: String = Constants.API_KEY
+    ): WeatherObject
+}
