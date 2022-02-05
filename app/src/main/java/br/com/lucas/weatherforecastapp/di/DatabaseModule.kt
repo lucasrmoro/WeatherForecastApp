93395d1c4312ee18708734ed6647135ec6d2b503
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-05 20:31:47 -0300

    Creating the Database Module to DI

---
+++ app/src/main/java/br/com/lucas/weatherforecastapp/di/DatabaseModule.kt
@@ -0,0 +1,36 @@
+package br.com.lucas.weatherforecastapp.di
+
+import android.content.Context
+import androidx.room.Room
+import br.com.lucas.weatherforecastapp.data.WeatherDao
+import br.com.lucas.weatherforecastapp.data.WeatherDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+    @Singleton
+    @Provides
+    fun provideAppContext(@ApplicationContext context: Context) = context
+
+    @Singleton
+    @Provides
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Singleton
+    @Provides
+    fun provideWeatherDatabase(weatherDao: WeatherDao, context: Context): WeatherDatabase =
+        Room.databaseBuilder(
+            context,
+            WeatherDatabase::class.java,
+            "weather-db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+}
