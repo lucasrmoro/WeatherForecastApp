24afd6e7cb8accffe26c8c413b48545801eed77d
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-09 00:23:52 -0300

    Adding the functions on repository to manipulate the unit settings

--- app/src/main/java/br/com/lucas/weatherforecastapp/repository/WeatherDbRepository.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/repository/WeatherDbRepository.kt
@@ -2,6 +2,7 @@ package br.com.lucas.weatherforecastapp.repository
 
 import br.com.lucas.weatherforecastapp.data.WeatherDao
 import br.com.lucas.weatherforecastapp.model.Favorite
+import br.com.lucas.weatherforecastapp.model.UnitSettings
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -12,4 +13,10 @@ class WeatherDbRepository @Inject constructor(private val weatherDao: WeatherDao
     suspend fun updateFavorite(favorite: Favorite) = weatherDao.updateFavorite(favorite)
     suspend fun deleteFavorite(favorite: Favorite) = weatherDao.deleteFavorite(favorite)
     suspend fun deleteAllFavorites() = weatherDao.deleteAllFavorites()
+
+    fun getUnitSettings(): Flow<List<UnitSettings>> = weatherDao.getUnitsSettings()
+    suspend fun insertUnitSettings(unitSettings: UnitSettings) = weatherDao.insertUnitSettings(unitSettings)
+    suspend fun updateUnitSettings(unitSettings: UnitSettings) = weatherDao.updateUnitSettings(unitSettings)
+    suspend fun deleteUnitSettings(unitSettings: UnitSettings) = weatherDao.deleteUnitSettings(unitSettings)
+    suspend fun deleteAllUnitsSettings() = weatherDao.deleteAllUnitSettings()
 }
