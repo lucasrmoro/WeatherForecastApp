9ea3d1125a0476b601c55b56e37b80cdfe0d335d
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-09 00:22:40 -0300

    Adding functions on Weather Dao to manipulate the unit settings

--- app/src/main/java/br/com/lucas/weatherforecastapp/data/WeatherDao.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/data/WeatherDao.kt
@@ -2,6 +2,7 @@ package br.com.lucas.weatherforecastapp.data
 
 import androidx.room.*
 import br.com.lucas.weatherforecastapp.model.Favorite
+import br.com.lucas.weatherforecastapp.model.UnitSettings
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -20,4 +21,20 @@ interface WeatherDao {
 
     @Query("DELETE FROM favorite_table")
     suspend fun deleteAllFavorites()
+
+    // Unit Settings table
+    @Query("SELECT * FROM settings_table")
+    fun getUnitsSettings(): Flow<List<UnitSettings>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUnitSettings(unitSettings: UnitSettings)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateUnitSettings(unitSettings: UnitSettings)
+
+    @Delete
+    suspend fun deleteUnitSettings(unitSettings: UnitSettings)
+
+    @Query("DELETE FROM settings_table")
+    suspend fun deleteAllUnitSettings()
 }
