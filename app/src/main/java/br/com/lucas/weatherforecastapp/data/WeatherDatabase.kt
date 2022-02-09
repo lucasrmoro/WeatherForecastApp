53ca0c1bf79488e14671abd624dc478e0f89d6b6
lucasrmoro <lucas.rmoro1@gmail.com>
2022-02-09 00:23:15 -0300

    Adding the Entity on Database and incrementing the version

--- app/src/main/java/br/com/lucas/weatherforecastapp/data/WeatherDatabase.kt
+++ app/src/main/java/br/com/lucas/weatherforecastapp/data/WeatherDatabase.kt
@@ -3,8 +3,9 @@ package br.com.lucas.weatherforecastapp.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import br.com.lucas.weatherforecastapp.model.Favorite
+import br.com.lucas.weatherforecastapp.model.UnitSettings
 
-@Database(entities = [Favorite::class], version = 1, exportSchema = false)
+@Database(entities = [Favorite::class, UnitSettings::class], version = 2, exportSchema = false)
 abstract class WeatherDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
